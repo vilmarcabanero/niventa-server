50d30faec580ad65b36528510469b2f6cba6f048
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-25 00:40:00 +0800

    Installed nodemon

--- src/index.server.js
+++ src/index.server.js
@@ -11,6 +11,12 @@ app.get('/', (req, res, next) => {
   })
 })
 
+app.post('/data', (req, res, next) => {
+  res.status(200).json({
+    message: req.body
+  })
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
 })
