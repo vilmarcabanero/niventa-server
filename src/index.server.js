2800ef3e488eb614036f1500ea8e96a5635e6397
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-25 02:21:50 +0800

    Created user model. Installed bcrypt.

--- src/index.server.js
+++ src/index.server.js
@@ -4,6 +4,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+//routes
+const userRoutes = require('./routes/user')
+
 //environment variable or you can say constants
 env.config()
 
@@ -22,18 +25,7 @@ mongoose
 	})
 
 app.use(bodyParser())
-
-app.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'Hello from Server',
-	})
-})
-
-app.post('/data', (req, res, next) => {
-	res.status(200).json({
-		message: req.body,
-	})
-})
+app.use('/api', userRoutes)
 
 app.listen(process.env.PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`)
---
+++ src/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+			min: 3,
+			max: 20,
+		},
+		lastName: {
+			type: String,
+			required: true,
+			trim: true,
+			min: 3,
+			max: 20,
+		},
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+			index: true,
+			lowercase: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			unique: true,
+		},
+		hash_password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			enum: ['user', 'admin'],
+			default: 'admin',
+		},
+		contactNumber: { type: String },
+		profilePicture: { type: String },
+	},
+	{ timestamps: true }
+)
+
+userSchema.virtual('password').set(password => {
+	this.hash_password = bcrypt.hashSync(password, 10)
+})
+
+userSchema.methods = {
+	authenticate: () => bcrypt.compareSync(password, this.hash_password),
+}
+
+module.exports = mongoose.model('User')
