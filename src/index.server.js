4d97453e02f555a5e657ae1f765aa93b29e80b4c
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-25 00:28:58 +0800

    Our first Hello API

--- src/index.server.js
+++ src/index.server.js
@@ -1,5 +1,15 @@
 const express = require('express')
+const env = require('dotenv')
 const app = express()
+
+//environment variable or you can say constants
+env.config()
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello from Server'
+  })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
